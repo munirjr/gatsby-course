c47d1e55beca4139798ecf6d53bc9f5bcc8abfb9
Junior <Munir.Junior@ao3tech.com>
2021-01-15 13:41:47 -0300

    upload all blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,27 @@
+require("dotenv").config()
+
+const queries = require("./src/utils/algolia_queries")
+
 module.exports = {
   siteMetadata: {
-    title: `My Blog`,
-    description: `A blog about frontend development and other cool stuff`,
+    title: `Professor X`,
+    position: `Backend Developer`,
+    description: `A blog about backend development and other cool stuff.`,
     author: `@myblog`,
+    siteUrl: `https://professorx.com.br`
   },
   plugins: [
+    `gatsby-plugin-transition-link`,
+    `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
+    //needs to be the first to work with gatsby-remark-images
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `uploads`,
+        path: `${__dirname}/static/assets/img`,
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -13,22 +29,63 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/posts`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: "gatsby-remark-relative-images",
+            options: {
+              name: "uploads"
+            },
+          },
+          {
+            resolve: "gatsby-remark-images",
+            options: {
+              maxWidth: 960,
+              linkImagesToOriginal: false
+            },
+          },
+          `gatsby-remark-lazy-load`,
+          `gatsby-remark-prismjs`
+        ],
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
+      resolve: `gatsby-plugin-algolia-search`,
+      options: {
+        appId: process.env.GATSBY_ALGOLIA_APP_ID,
+        apiKey: process.env.ALGOLIA_ADMIN_KEY,
+        indexName: process.env.GATSBY_ALGOLIA_INDEX_NAME,
+        queries,
+        chunkSize: 10000,
+        enablePartialUpdates: true,
+      },
+    },
+    {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Professor X`,
+        short_name: `Professor X`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#16202C`,
+        theme_color: `#16202C`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-plugin-sitemap`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,24 @@
+const links = [
+    {
+        label: 'Github',
+        url: 'https://github.com'
+    },
+    {
+        label: 'Twitter',
+        url: 'https://twitter.com'
+    },
+    {
+        label: 'Youtube',
+        url: 'https://youtube.com'
+    },
+    {
+        label: 'Unsplash',
+        url: 'https://unsplash.com'
+    },
+    {
+        label: 'Instagram',
+        url: 'https://instagram.com'
+    }
+]
+
+export default links
